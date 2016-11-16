c074a5d3909e66697ac203f5a4c6a5e21d94461a
mastohhh <mastohhh@users.noreply.github.com>
2016-11-16 18:50:29 +0100

    update install script

--- installConfig.sh
+++ installConfig.sh
@@ -12,63 +12,62 @@ else
     brew update
 fi
 
-
 #
-# Brew
+# Install Brews
 #
 
-brew install fish
-brew install node
+brew install cask
 brew install mas
+brew install rbenv
 brew install carthage
 
 #
-# Cask
+# Install Gems
 #
-brew cask install dropbox
-brew cask install sublime-text
-brew cask install alfred
-brew cask install google-chrome
+
+gem install bundler
+
+#
+# Install Cask Apps
+#
+
 brew cask install vlc
-brew cask install utorrent
-brew cask install tower
-brew cask install spotify
-brew cask install slack
-brew cask install sketch
-brew cask install postman
-brew cask install google-drive
-brew cask install flux
+brew cask install google-chrome
+brew cask install firefox
+brew cask install opera
+brew cask install transmission
 brew cask install flash-player
-brew cask install macdown
-brew cask install subl
-brew cask install iterm2
+brew cask install sublime-text
+brew cask install textmate
+brew cask install tower
+brew cask install filezilla
+brew cask install google-drive
+brew cask install dropbox
+brew cask install spotify
 
 #
-# MAS
+# Install AppStore
 #
 
-mas signin cflorion@gmail.com
-
-# Copied
-mas install 1026349850
-
-# Flexiglass
-mas install 426410278
-
-# Kaleidoscope
-mas install 587512244
-
-# Xcode
-mas install 497799835
-
-# Slack
-mas install 803453959
-
-# Clear
-mas install 504544917
-
-# Fantastical 2
-mas install 975937182
-
-# Mindnode 2
-mas install 992076693
+mas install 492068728 # Yummy FTP (1.11.11)
+mas install 409183694 # Keynote (7.0.5)
+mas install 409907375 # Remote Desktop (3.8)
+mas install 747648890 # Telegram (2.27)
+mas install 425424353 # The Unarchiver (3.11.1)
+mas install 1050660598 # zik-manager (1.4)
+mas install 813162820 # LiveQuartz Lite (2.5.12)
+mas install 442160987 # Flycut (1.5)
+mas install 417250177 # Sparrow (1.6.4)
+mas install 490152466 # iBooks Author (2.5)
+mas install 880001334 # Reeder (3.0.1)
+mas install 409203825 # Numbers (4.0.5)
+mas install 497799835 # Xcode (8.1)
+mas install 409201541 # Pages (6.0.5)
+mas install 411277085 # Resizer (1.4)
+mas install 408981434 # iMovie (10.1.3)
+mas install 1091189122 # Bear (1.0.2)
+mas install 493527692 # RC Mini Racers (2.8)
+mas install 803453959 # Slack (2.3.1)
+mas install 714196447 # MenuBar Stats (2.2)
+mas install 775737590 # iA Writer (3.2.6)
+mas install 991607914 # BTCGuild Pool (3.5)
